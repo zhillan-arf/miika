6bc41a2771a27aba8a01b72acb8ee0397ed77420
zhillan-arf <zhilanar@gmail.com>
2024-06-26 23:59:20 +0700

    docs

--- test/test.py
+++ test/test.py
@@ -1,18 +1,26 @@
 import json
 import requests
 import re
+import datetime
 
 with open('prompt.txt', 'r') as file:
     prompt = file.read()
 
-userName = "danny"
-secName = "mist"
+userName = "Danny"
+secName = "Mist"
+date = datetime.datetime.now().strftime("%A, %d %B %Y")
+
+with open('coreGuides.txt', 'r') as file:
+    coreGuides = file.read()
 
 with open('contextGuides.txt', 'r') as file:
     contextGuides = file.read()
 
 with open('contextEpisodes.txt', 'r') as file:
     contextEpisodes = file.read()
+
+with open('secIntent.txt', 'r') as file:
+    secIntent = file.read()
 
 with open('recentChats.txt', 'r') as file:
     recentChats = file.read()
@@ -24,11 +32,14 @@ with open('recentChats.txt', 'r') as file:
 #     return ''.join([f"{idx}. {text}\n" for idx, text in enumerate(texts)]).rstrip('\n')
 
 contexts = {
+    'coreGuides': coreGuides,
     'contextGuides': contextGuides,
     'contextEpisodes': contextEpisodes,
     'recentChats': recentChats,
     'userName': userName,
-    'secName': secName
+    'secName': secName,
+    'secIntent': secIntent,
+    'date': date
 }
 
 for key, value in contexts.items():
@@ -37,16 +48,16 @@ for key, value in contexts.items():
 
 print(prompt)
 
-data = { 'prompt': prompt }
+# data = { 'prompt': prompt }
 
-url = 'http://localhost:3001/api/infer'
-headers = {'Content-Type': 'application/json'}
+# url = 'http://localhost:3001/api/infer'
+# headers = {'Content-Type': 'application/json'}
 
-response = requests.post(url, headers=headers, json=data)
+# response = requests.post(url, headers=headers, json=data)
 
-# Check the response status code
-if response.status_code == 200:
-    print('Response:', response.json())
-else:
-    print(f'Request failed with status code: {response.status_code}')
+# # Check the response status code
+# if response.status_code == 200:
+#     print('Response:', response.json())
+# else:
+#     print(f'Request failed with status code: {response.status_code}')
 
