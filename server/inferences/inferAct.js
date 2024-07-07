c89c064af9bb598bf59447668b64441c9a9d89f8
zhillan-arf <zhilanar@gmail.com>
2024-07-08 00:17:39 +0700

    JSONified more prompts

--- server/inferences/inferAct.js
+++ server/inferences/inferAct.js
@@ -1,18 +1,18 @@
 import infer from "./infer.js";
 import makePrompt from "../functions/makePrompt.js";
+import path from 'path';
 
 const inferAct = async (recentChats) => {
     const contexts = { recentChats: recentChats }
-    const localPath = 'inferences/inferAct';
-    const actPrompt = await makePrompt(contexts, localPath);
+    const promptPath = path.resolve('prompts/inferences/inferAct.json');
+    const actPrompt = await makePrompt(contexts, promptPath);
     
-    let act = true;
     try {
         const act = await infer(actPrompt);
         return act;
     
     } catch (err) {
-        console.error(err);
+        console.error(err.stack);
         return null;
     }
 }
